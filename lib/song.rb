c326a6cabfcd38c4d955e26fea6325d19516743e
Gino Desangles <blushingbox@yahoo.com>
2019-04-03 23:57:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,7 @@
 class Song
-  attr_accessor :title
-  
-  def initialize(title)
+  attr_accessor :title, :author
+  def initialize=(title, author)
     @title = title
+    @author = author
   end
 end
